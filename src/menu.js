63807f9e545e590ce69d70c788fa29967cf37ae8
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 <erushblu2@gmail.com>
2026-07-11 22:15:59 -0300

    problemas na batalha de turnos

--- src/menu.js
+++ src/menu.js
@@ -2,8 +2,8 @@ import { intro, select, note } from "@clack/prompts";
 import colorize from "strcolorize";
 
 export async function battle(pokemon1, pokemon2) {
-  while (pokemon1.hp > 0 || pokemon2.hp > 0) {
-    const duelists = [pokemon1.name, pokemon2.name];
+  while (pokemon1.hp > 0 && pokemon2.hp > 0) {
+    const duelists = [pokemon1, pokemon2];
     let turnCount = 0;
 
     intro(colorize(`#inverse[Turno ${++turnCount}]`));
@@ -21,5 +21,11 @@ HP: ${pokemon2.hp}
         { value: 2, label: "[ 2 ] Defender" },
       ],
     });
+
+    if (options === 1) {
+      const damage = duelists[0].attack(duelists[1]);
+
+      note(`${duelists[0].name} atacou e causou ${damage} de dano!`);
+    }
   }
 }
--- src/structures/Pokemon.js
+++ src/structures/Pokemon.js
@@ -4,19 +4,22 @@ export class Pokemon {
     this.hp = hp;
     this.attack = attack;
     this.defense = defense;
+    this.isDefend = false;
   }
 
   attack(enemy) {
     const damage = this.attack - enemy.defense;
+
+    if (enemy.isDefend) {
+      enemy.hp -= enemy.defense >= this.attack ? 0 : Math.floor(damage / 2);
+    }
+
     enemy.hp -= enemy.defense >= this.attack ? 0 : damage;
 
     return damage;
   }
 
   defend(enemy) {
-    const enemyDamage = enemy.attack - this.defense;
-    this.hp -= this.defense >= enemy.attack ? 0 : enemyDamage;
-
-    return enemyDamage;
+    this.isDefend = true;
   }
 }
